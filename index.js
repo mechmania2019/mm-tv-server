bc4cd6148ebe099f24c10aa620c30ec5a367d752
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 19:47:16 -0500

    Send team names in headers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { promisify } = require("util");
 const mongoose = require("mongoose");
 const AWS = require("aws-sdk");
 const { send, buffer } = require("micro");
-const { Match } = require("mm-schemas")(mongoose);
+const { Match, Script } = require("mm-schemas")(mongoose);
 
 const MIN_MATCH_LENGTH = 6;
 const LAST_N_MATCHES_TO_PICK_FROM = 120;
@@ -36,9 +36,23 @@ module.exports = async (req, res) => {
     .sort("-_id")
     .skip(random)
     .exec();
-  console.log(`${match.key} - Got a record`);
+  console.log(`${match.key} - Parsing match key`);
+  const [s1, s2] = match.key.slice("logs/".length).split(":");
+  console.log(`${s1} v ${s2} - Getting team names`);
+  const [t1, t2] = await Promise.all(
+    [s1, s2].map(async s => {
+      const script = await Script.findOne({ key: s })
+        .populate("owner")
+        .exec();
+      return script.owner.name;
+    })
+  );
 
-  console.log(`${match.key} - Downloading it from s3`);
+  console.log(`${t1} v ${t2} - Sending headers`);
+  res.setHeader("X-team-1", t1);
+  res.setHeader("X-team-2", t2);
+
+  console.log(`${t1} v ${t2} - Streaming match ${match.key} from s3`);
   try {
     return send(res, 200, s3.getObject({ Key: match.key }).createReadStream());
   } catch (e) {
