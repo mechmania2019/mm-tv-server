6723263cd60da0c6d836dc5736366ba90266cf81
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 19:14:57 -0500

    Select only recent matches that have 6 moves

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const AWS = require("aws-sdk");
 const { send, buffer } = require("micro");
 const { Match } = require("mm-schemas")(mongoose);
 
+const MIN_MATCH_LENGTH = 6;
+const LAST_N_MATCHES_TO_PICK_FROM = 120;
+
 mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
@@ -17,21 +20,28 @@ const getObject = promisify(s3.getObject.bind(s3));
 module.exports = async (req, res) => {
   // TODO: Prefer picking newer matches
   console.log(`Getting a random record from mongo`);
-  const count = await Match.count().exec();
+  const count = await Match.count()
+    .where("length")
+    .gt(MIN_MATCH_LENGTH)
+    .limit(LAST_N_MATCHES_TO_PICK_FROM)
+    .sort("-_id")
+    .exec();
   console.log(`There are ${count} matches to pick from`);
   const random = Math.floor(Math.random() * count);
   console.log(`Grabbing record ${random}`);
   const match = await Match.findOne()
+    .where("length")
+    .gt(MIN_MATCH_LENGTH)
+    .limit(LAST_N_MATCHES_TO_PICK_FROM)
+    .sort("-_id")
     .skip(random)
     .exec();
   console.log(`${match.key} - Got a record`);
 
   console.log(`${match.key} - Downloading it from s3`);
   try {
-    return send(res, 200, s3
-      .getObject({ Key: match.key })
-      .createReadStream());
+    return send(res, 200, s3.getObject({ Key: match.key }).createReadStream());
   } catch (e) {
-    return send(res, 500, "Error when getting the logfile")
+    return send(res, 500, "Error when getting the logfile");
   }
 };
